3634776c21b91687bee68477bf2257c2ba11bab7
ginormousnut <92399323+ginormousnut@users.noreply.github.com>
2022-10-15 16:45:47 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,33 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace zadacha1
+namespace zadacha1__n2
 {
     class Program
     {
         static void Main()
         {
-            int n;
+            int n=0;
             double x;
-            double s=Math.PI/2;
+            double s = Math.PI / 2;
             double e;
             double a;
             Console.WriteLine("Введите переменную>");
-            x=Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введите количество членов ряда>");
-            n= Convert.ToInt32(Console.ReadLine());
-            for(double i=1; i<=n; i++)
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите погрешность>");
+            e = Convert.ToDouble(Console.ReadLine());
+            a= Math.Pow(-1, 1) * Math.Pow(x, 2 * 1 - 1) / (2 * 1 - 1);
+            while (Math.Abs(a) > e)
             {
-                s += Math.Pow(-1, i) * Math.Pow(x, 2 * i - 1) / (2 * i - 1);
+                n++;
+                s += Math.Pow(-1, n) * Math.Pow(x, 2 * n - 1) / (2 * n - 1);
+                a= Math.Pow(-1, n+1) * Math.Pow(x, 2 * (n+1) - 1) / (2 * (n+1) - 1);
             }
             Console.Write("Полученная частичная сумма ");
             Console.WriteLine(s);
-            e = Math.Abs(s - (Math.PI/2 - Math.Atan(x)));
-            Console.Write("Полученная погрешность ");
-            Console.WriteLine(e);
-            a = Math.Pow(-1, n) * Math.Pow(x, 2 * n - 1) / (2 * n - 1);
-            Console.Write("Полученная последний член ");
-            Console.WriteLine(a);
+            
+            Console.Write("Полученное количество членов ");
+            Console.WriteLine(n);
+            e = e / 10;
+            s = Math.PI / 2;
+            n = 0;
+            a = Math.Pow(-1, 1) * Math.Pow(x, 2 * 1 - 1) / (2 * 1 - 1);
+            while (Math.Abs(a) > e)
+            {
+                n++;
+                s += Math.Pow(-1, n) * Math.Pow(x, 2 * n - 1) / (2 * n - 1);
+                a = Math.Pow(-1, n + 1) * Math.Pow(x, 2 * (n + 1) - 1) / (2 * (n + 1) - 1);
+            }
+            Console.Write("Полученная частичная сумма для погрешности, отличающееся на порядок ");
+            Console.WriteLine(s);
+
+            Console.Write("Полученное количество членов для погрешности, отличающееся на порядок ");
+            Console.WriteLine(n);
+
             Console.ReadKey();
         }
     }
